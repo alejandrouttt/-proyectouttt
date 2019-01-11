888163ab6b646831ba4cdcd94bbe876184479b25
EnriqueGante <46576405+EnriqueGante@users.noreply.github.com>
2019-01-11 12:03:16 -0600

    enr
    hoka

---
+++ CargaAcademicaUttt_Proyecto/CargaAcademicaUttt_Proyecto/Vistas/ExepcionCatedratico_Estadias.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargaAcademicaUttt_Proyecto.Vistas
+{
+    public partial class ExepcionCatedratico_Estadias : Form
+    {
+        public ExepcionCatedratico_Estadias()
+        {
+            InitializeComponent();
+        }
+    }
+}
